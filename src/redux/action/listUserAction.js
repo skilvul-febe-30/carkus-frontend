0be350dad01b3167ceda01d7f7d477252301dfa2
Sukiya Rizkiyani <85722923+Sukiya2003@users.noreply.github.com>
2022-12-02 10:20:34 +0700

    update userAction

--- src/redux/action/listUserAction.js
+++ src/redux/action/listUserAction.js
@@ -1,27 +1,50 @@
-import axios from "axios"
+import axios from "axios";
 
-export const GET_USER = 'GET_USER'
+export const GET_USER = "GET_USER";
+export const PUT_USER = "PUT_USER";
 
-function sucsessGet(params){
-    return{
-        type: GET_USER,
-        payload: params
-    }
+function sucsessGet(params) {
+  return {
+    type: GET_USER,
+    payload: params,
+  };
 }
 
 export const getUser = () => {
-    return async(dispatch) => {
-
-        const result = await axios("https://634840db0484786c6e95e220.mockapi.io/Data")
-        dispatch(sucsessGet(result.data))
-    }
-}
+  return async (dispatch) => {
+    const result = await axios(
+      "https://634840db0484786c6e95e220.mockapi.io/Data"
+    );
+    dispatch(sucsessGet(result.data));
+  };
+};
 
 export const postUser = (data) => {
-    return async(dispatch) => {
-        axios.post("https://634840db0484786c6e95e220.mockapi.io/Data",
-        data).then((res)=>{
-            alert("akun berhasil dibuat")
-        })
-    }
-}
+  return async (dispatch) => {
+    axios
+      .post("https://634840db0484786c6e95e220.mockapi.io/Data", data)
+      .then((res) => {
+        alert("Akun berhasil dibuat");
+      });
+  };
+};
+
+export const putUser = (data) => {
+  const uId = localStorage.getItem('id')
+  return async (dispatch) => {
+    axios
+      .put(`https://634840db0484786c6e95e220.mockapi.io/Data/${uId}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+      })
+      .then((result) => {
+        dispatch({
+          type : PUT_USER,
+          payload: {
+              data: result.data,
+          }
+      })
+      alert("Akun berhasil diubah")
+      });
+  };
+};
